0f5fbeb05532015691fd78f9209a9fb7331358ce
jokereven125 <zhou125disorder@163.com>
2021-09-10 12:31:51 +0800

    2021/9/10 12:31 update

--- config/index.js
+++ config/index.js
@@ -1,66 +1,60 @@
 const config = {
-  projectName: 'TaroTest',
-  date: '2021-8-23',
+  projectName: "TaroTest",
+  date: "2021-8-23",
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
-    828: 1.81 / 2
+    828: 1.81 / 2,
   },
-  sourceRoot: 'src',
-  outputRoot: 'dist',
-  plugins: [],
-  defineConstants: {
-  },
+  sourceRoot: "src",
+  outputRoot: "dist",
+  plugins: ["@tarojs/plugin-html"],
+  defineConstants: {},
   copy: {
-    patterns: [
-    ],
-    options: {
-    }
+    patterns: [],
+    options: {},
   },
-  framework: 'react',
+  framework: "react",
   mini: {
     postcss: {
       pxtransform: {
         enable: true,
-        config: {
-
-        }
+        config: {},
       },
       url: {
         enable: true,
         config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
+          limit: 1024, // 设定转换尺寸上限
+        },
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
   },
   h5: {
-    publicPath: '/',
-    staticDirectory: 'static',
+    publicPath: "/",
+    staticDirectory: "static",
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {
-        }
+        config: {},
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
-  }
-}
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
+  },
+};
 
 module.exports = function (merge) {
   if (process.env.NODE_ENV === 'development') {
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,48 +1,86 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
+import { Button, Text, View } from "@tarojs/components";
+import { Component } from "react";
+import { connect } from "react-redux";
+import { add, asyncAdd, minus } from "../../actions/counter";
+import "./index.scss";
 
-import { add, minus, asyncAdd } from '../../actions/counter'
-
-import './index.scss'
-
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
+@connect(
+  ({ counter }) => ({
+    counter,
+  }),
+  (dispatch) => ({
+    add() {
+      dispatch(add());
+    },
+    dec() {
+      dispatch(minus());
+    },
+    asyncAdd() {
+      dispatch(asyncAdd());
+    },
+  })
+)
 class Index extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps);
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+      <View className="index">
+        <Button className="add_btn" onClick={this.props.add}>
+          +
+        </Button>
+        <Button className="dec_btn" onClick={this.props.dec}>
+          -
+        </Button>
+        <Button className="dec_btn" onClick={this.props.asyncAdd}>
+          async
+        </Button>
+        <View>
+          <Text
+            style={{
+              textAlign: "center",
+              width: "100%",
+              display: "block",
+              fontFamily: "宋体",
+            }}
+          >
+            {this.props.counter.num}
+          </Text>
+        </View>
+        <View>
+          <Text
+            style={{
+              textAlign: "center",
+              width: "100%",
+              display: "block",
+              fontFamily: "宋体",
+            }}
+          >
+            Hello, World
+          </Text>
+        </View>
+        <View>
+          <img
+            style={{ width: "100%" }}
+            src="https://blog.code520.com.cn/medias/banner/categories.jpg"
+          ></img>
+        </View>
+        <audio
+          src="http://m701.music.126.net/20210910124740/4de207873619bd7da9d0574759b10f11/jdymusic/obj/wo3DlMOGwrbDjj7DisKw/9220995975/a82c/c8f1/c240/74c34f5ed76bbb99c3022948717e56e4.mp3"
+          controls
+          loop
+        ></audio>
       </View>
-    )
+    );
   }
 }
 
-export default Index
-
+export default Index;
